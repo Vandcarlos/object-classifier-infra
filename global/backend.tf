6f96630e8bb5201f8a30c9352d433788f4f7eb60
Vand <vandcarlosjunior@gmail.com>
2025-11-15 13:30:43 -0300

    Changed backend locks

--- global/backend.tf
+++ global/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "tfstate-vandcarlos-object-classifier"
     key            = "object-classifier-infra/global/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "tf-state-locks"
+    dynamodb_table = "tfstate-ml-locks"
     encrypt        = true
   }
 }
