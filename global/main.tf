ddef89470c65b741ad507e70ced847c714e93fc9
Vand <vandcarlosjunior@gmail.com>
2025-11-15 01:04:25 -0300

    Added env variable

--- global/main.tf
+++ global/main.tf
@@ -32,7 +32,7 @@ module "iam_github" {
 module "artifacts" {
   source = "./storage/artifacts"
 
-  artifacts_bucket_name = "ml-artifacts-${data.aws_caller_identity.me.account_id}}"
+  artifacts_bucket_name = "ml-artifacts-${data.aws_caller_identity.me.account_id}}=${env}"
 
   producers = {
     model = {
